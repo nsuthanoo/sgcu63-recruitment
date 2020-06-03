e89987b8ff52ad146a2937d49b05b01754abdf60
nsuthanoo <nsuthanoo17@gmail.com>
2020-06-04 01:48:57 +0700

    Change bootstrap CSS link and some typos

--- Prob#1 - The scraper/Problem1.py
+++ Prob#1 - The scraper/Problem1.py
@@ -12,9 +12,9 @@ def get_baan_url(url):
 
 #Get baan names and slogans from the list of URLs
 def get_slogan(url_list):
-    #Create dictionary to store baan names and slogans
+    #Create dictionary to store baans' names and slogans
     baan_slogan={}
-    #Loop inside the url list to scrape each page inside the list
+    #Iterate through the url list to scrape each page in the list
     for url in url_list:
         info=requests.get(url)
         #Find baan's name in Thai
@@ -27,12 +27,12 @@ def get_slogan(url_list):
 def write_html(baan_slogan):
     #Create a html file
     with open('Prob#1 - The scraper/table.html','w', encoding='utf8') as html:
-        #Write header to file
+        #Write header to the file
         html.write('''<!DOCTYPE html>
     <html>
         <head>
             <title>Baan Rub Nong Slogan Table</title>
-            <link href="http://www.amiels.lnw.mn/css/bootstrap-4.3.1.css" rel="stylesheet">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         </head>
         <body  class="bg-secondary">
             <div class="container">
@@ -61,6 +61,6 @@ def write_html(baan_slogan):
 def create_table(url):
     write_html(get_slogan(get_baan_url(url)))
 
-#The URL to javascript file that has all baan's names/links
+#The link to javascript file that contains all baans' names/links to their pages
 url='https://rubnongkaomai.com/component---src-pages-baan-js-b2ff2cb9f7799cf2b1de.js'
 create_table(url)

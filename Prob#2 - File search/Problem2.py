e01477faabd4a35e717ecaa762a1feba66707b71
nsuthanoo <nsuthanoo17@gmail.com>
2020-06-07 11:49:41 +0700

    Revert "Fix grammatical errors in some comments"
    This reverts commit f117b134a0cd9e4907238492c67702f610b567be.

--- Prob#2 - File search/Problem2.py
+++ Prob#2 - File search/Problem2.py
@@ -8,11 +8,11 @@ def search(file_name, JSON_file):
     with open(JSON_file,'r') as Jfile:
         directory=json.loads(Jfile.read())
     result=[]
-    #Iterate through all folders(In case there're more than one)
+    #Loop through all folders(In case there're more than one)
     for parent_folder in directory.keys():
         #Get the list of files in the folder
         files_list=directory[parent_folder]["_files"]
-        #Add the directory to result if the file is in the folder
+        #Add to result if the file is in the folder
         if file_name in files_list:
             result.append("/"+parent_folder+"/"+file_name)
         #Go through all subfolders
@@ -20,16 +20,16 @@ def search(file_name, JSON_file):
             if sub_folder != "_files":
                  #Get the list of files in the subfolder
                 files_list=directory[parent_folder][sub_folder]["_files"]
-                #Add the directory to result if the file is in the subfolder
+                #Add to result if the file is in the subfolder
                 if file_name in files_list:
                     result.append("/"+parent_folder+"/"+sub_folder+"/"+file_name)
-    #Print out the result
+    #Print out result
     print(result)
 
 
-#State the JSON file to use
+#Choose JSON file to use
 JSON_file="Prob#2 - File Search/file.json"
-#State the name of the file to search for
+#Choose the name of file to search for
 file_to_search='file1'
 
 search(file_to_search,JSON_file)
